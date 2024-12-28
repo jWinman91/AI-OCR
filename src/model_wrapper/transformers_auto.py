78a6dd53d3b2099e6082e85ef9004542fe890138
jWinman91 <jwinkelm@tcd.ie>
2024-12-28 17:38:00 +0100

    fixed parameter bug in transformers model wrapper

--- src/model_wrapper/transformers_auto.py
+++ src/model_wrapper/transformers_auto.py
@@ -5,10 +5,16 @@ from typing import Optional
 
 
 class TransformersAuto:
-    def __init__(self, model_name: str, **kwargs) -> None:
-        self._model = AutoModel.from_pretrained(model_name, torch_dtype=torch.bfloat16, **kwargs)
-        self._model = self._model.eval().cuda()
-        self._tokenizer = AutoTokenizer.from_pretrained(self._model, trust_remote_code=True)
+    def __init__(self, config_dict: dict) -> None:
+        construct_params = config_dict.get("construct_params", {})
+        construct_params["attn_implementation"] = "sdpa"
+        construct_params["torch_dtype"] = torch.bfloat16
+        construct_params["trust_remote_code"] = True
+
+        self._model = AutoModel.from_pretrained(config_dict.get("model_path"), **construct_params)
+        print(self._model)
+        #self._model = self._model.eval().cuda()
+        self._tokenizer = AutoTokenizer.from_pretrained(config_dict.get("model_path"), trust_remote_code=True)
 
     def predict(self, text: str, image: object, parameters: Optional[dict] = None) -> str:
         messages = [{
--- src/model_wrapper/transformers_causal_llm.py
+++ src/model_wrapper/transformers_causal_llm.py
@@ -7,16 +7,17 @@ from typing import Optional
 
 
 class TransformersCasualLLM:
-    def __init__(self, model_name: str, **kwargs) -> None:
+    def __init__(self, config_dict: dict) -> None:
         """
         Wrapper to load and use LLMs via vLLM for prediction.
         Args:
             model_name: name of LLM
             **kwargs: dictionary of arguments
         """
-        self._model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", **kwargs)
-        self._tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
-        self._processor = AutoProcessor.from_pretrained(model_name, trust_remote_code=True)
+        construct_params = config_dict.get("construct_params", {})
+        self._model = AutoModelForCausalLM.from_pretrained(config_dict.get("model_path"), device_map="auto", **construct_params)
+        self._tokenizer = AutoTokenizer.from_pretrained(config_dict.get("model_path"), use_fast=True)
+        self._processor = AutoProcessor.from_pretrained(config_dict.get("model_path"), trust_remote_code=True)
 
     def predict(self, text: str, image: Optional[object] = None, parameters: Optional[dict] = None) -> str:
         messages = [{
